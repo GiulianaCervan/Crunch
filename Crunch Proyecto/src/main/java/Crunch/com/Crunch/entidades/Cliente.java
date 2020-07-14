35f017a25398241a0069e6d731a9bf60bbabb664
Giuliana Cervan <giuliana.cervan@gmail.com>
2020-07-14 18:00:28 -0300

    Cliente

--- Crunch Proyecto/src/main/java/Crunch/com/Crunch/entidades/Cliente.java
+++ Crunch Proyecto/src/main/java/Crunch/com/Crunch/entidades/Cliente.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Crunch.com.Crunch.entidades;
 
-/**
- *
- * @author giuli
- */
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
 public class Cliente {
+    
+    @Id
+    private String mail;
+    
+    private String clave;
+    private String nombre;
+    private String apellido;
+    private String domicilio;
+    private String telefono;
+    private Integer puntos;
+    private List<Cupon> cuponPromo;
+    private List<CuponDeCanje> cuponCanje;
+    private List<Raspadita> raspaditas;
+    private List<Valoracion> valoraciones;
     
 }
