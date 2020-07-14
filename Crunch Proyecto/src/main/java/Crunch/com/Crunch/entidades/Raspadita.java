d509bcfd450e85d157f991410456d2b132d236cb
cristian1098 <67169924+cristian1098@users.noreply.github.com>
2020-07-14 19:03:53 -0300

    entidades

---
+++ Crunch Proyecto/src/main/java/Crunch/com/Crunch/entidades/Raspadita.java
@@ -0,0 +1,40 @@
+package Crunch.com.Crunch.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Raspadita {
+
+    @Id
+    private String identificador;
+    private Integer puntuacion;
+
+    public Raspadita() {
+    }
+
+    public Raspadita(String identificador, Integer puntuacion) {
+        this.identificador = identificador;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public Integer getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Integer puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public void canje() {
+
+    }
+}
