c4c6fabf4c0f23f52caf32437b3f046f4cc78c00
Lautaro_Heredia <lauta9710@gmail.com>
2020-07-16 20:02:27 -0300

    Me había olvidado de guardar el archivo de Seguridad cliente

--- Crunch/src/main/java/Crunch/SeguridadClienteConfiguracion.java
+++ Crunch/src/main/java/Crunch/SeguridadClienteConfiguracion.java
@@ -5,10 +5,52 @@
  */
 package Crunch;
 
+import Crunch.servicios.ServicioCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 /**
  *
  * @author lauta
  */
-public class SeguridadClienteConfiguracion {
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadClienteConfiguracion extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ServicioCliente servicioCliente;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(servicioCliente)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
     
+    @Override //hibrido del que nos pasó el Agus y el video de egg
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin().and()
+                .authorizeRequests()
+                .antMatchers("/css/*","/js/*","/img/*")
+                .permitAll()
+                .and().formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("clave")
+                .defaultSuccessUrl("/inicio")
+                .and().logout()
+                .logoutUrl("/logout")
+                .permitAll()
+                .and().csrf()
+                .disable();
+    }
+
 }
