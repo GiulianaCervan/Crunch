af24c83abf500e18a727a917bc7c1da83107631a
Duilio-Ditommasi <duilio_07@hotmail.com>
2020-07-21 17:27:55 -0300

    pom arreglado

--- Crunch/src/main/java/Crunch/servicios/ServicioFoto.java
+++ Crunch/src/main/java/Crunch/servicios/ServicioFoto.java
@@ -3,7 +3,7 @@ package Crunch.servicios;
 
 import Crunch.entidades.Foto;
 import Crunch.repositorios.FotoRepositorio;
-import errores.ErrorService;
+
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,46 +16,46 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ServicioFoto {
-    @Autowired
-    private FotoRepositorio FotoRepositorio;
-
-
-    public Foto guardar(MultipartFile archivo)throws ErrorService, IOException{
-        if (archivo !=null){
-
-          try{
-            Foto foto = new Foto();
-            foto.setMime(archivo.getContentType());
-            foto.setNombre(archivo.getName());
-            foto.setContedido(archivo.getBytes());
-            return FotoRepositorio.save(foto);  
-        } catch(Exception e){
-                System.err.println(e.getMessage());
-       }
-    }else{
-            return null;
-        }
-        public Foto actualizar (String idFoto, MultipartFile archivo) throws ErrorService{
-    
-if (archivo !=null){
-
-          try{
-            Foto foto = new Foto();
-            
-            if(idFoto !=null){
-                Optional<foto> respuesta = fotoRepositorio.findById(idFoto);
-                if(respuesta.isPresent()){
-                    foto = respuesta.get();
-                }
-            }
-            foto.setMime(archivo.getContentType());
-            foto.setNombre(archivo.getName());
-            foto.setContedido(archivo.getBytes());
-            return FotoRepositorio.save(foto);  
-        } catch(Exception e){
-                System.err.println(e.getMessage());
-        }
-          return null;
+//    @Autowired
+//    private FotoRepositorio FotoRepositorio;
+//
+//
+//    public Foto guardar(MultipartFile archivo)throws ErrorService, IOException{
+//        if (archivo !=null){
+//
+//          try{
+//            Foto foto = new Foto();
+//            foto.setMime(archivo.getContentType());
+//            foto.setNombre(archivo.getName());
+//            foto.setContedido(archivo.getBytes());
+//            return FotoRepositorio.save(foto);  
+//        } catch(Exception e){
+//                System.err.println(e.getMessage());
+//       }
+//    }else{
+//            return null;
+//        }
+//        public Foto actualizar (String idFoto, MultipartFile archivo) throws ErrorService{
+//    
+//if (archivo !=null){
+//
+//          try{
+//            Foto foto = new Foto();
+//            
+//            if(idFoto !=null){
+//                Optional<foto> respuesta = fotoRepositorio.findById(idFoto);
+//                if(respuesta.isPresent()){
+//                    foto = respuesta.get();
+//                }
+//            }
+//            foto.setMime(archivo.getContentType());
+//            foto.setNombre(archivo.getName());
+//            foto.setContedido(archivo.getBytes());
+//            return FotoRepositorio.save(foto);  
+//        } catch(Exception e){
+//                System.err.println(e.getMessage());
+//        }
+//          return null;
 }
 
 
